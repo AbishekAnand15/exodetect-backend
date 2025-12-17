192667bb2d6db7a2b806f09385dddcd5f14456a0
Abishek Xavier A <118706942+AbishekAnand15@users.noreply.github.com>
2025-12-18 01:12:40 +0530

    Change CORS settings to allow all origins

--- main.py
+++ main.py
@@ -6,12 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "https://abishekanand15.github.io",   
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],  
     allow_methods=["*"],
     allow_headers=["*"],
 )
